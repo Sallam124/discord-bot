7d33782e666018e9b9b723bcccf2944dfeeee3bf
Sallam124 <153922132+Sallam124@users.noreply.github.com>
2025-06-20 21:37:26 +0300

    fuck spotify

--- commands/play.js
+++ commands/play.js
@@ -1,27 +1,17 @@
 const { QueryType } = require('distube');
-const { getSpotifyTrackQuery } = require('../utils/spotifyHelper');
 
 module.exports = {
   name: 'play',
-  description: 'Play a song from YouTube, Spotify, or search keywords',
+  description: 'Play a song from YouTube or search keywords',
   async execute(message, args, client) {
     // Check if user is in a voice channel
     const voiceChannel = message.member.voice.channel;
     if (!voiceChannel) return message.reply('You need to be in a voice channel to play music!');
-    if (!args.length) return message.reply('Please provide a YouTube/Spotify link or search keywords.');
+    if (!args.length) return message.reply('Please provide a YouTube link or search keywords.');
 
     let query = args.join(' ');
-    // If Spotify URL, extract metadata and search YouTube
-    if (/open\.spotify\.com\//.test(query)) {
-      try {
-        query = await getSpotifyTrackQuery(query);
-        if (!query) return message.reply('Could not extract track info from Spotify link.');
-      } catch (err) {
-        return message.reply('Error processing Spotify link.');
-      }
-    }
 
-    // Play the song using DisTube
+    // Play the song or playlist using DisTube
     try {
       await client.distube.play(voiceChannel, query, {
         member: message.member,
